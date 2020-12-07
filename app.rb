fe4a44141d2e4b6b8d4217be074ba9c9d7a21911
Aman Tank <amantank187@gmail.com>
2020-12-07 17:00:48 +0000

    Moving HTML code into views folder

--- app.rb
+++ app.rb
@@ -10,9 +10,5 @@ get '/secret' do
 end
 
 get '/cat' do
-    "<div> 
-    <div style='border: 3px solid blue'>
-     
-    <img src= 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*'>
-     </div>"
-  end
+  erb(:index)
+end
