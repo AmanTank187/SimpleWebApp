a0fcadbd0dc0d71d56c120b7a79556f4df26b887
Lisa <bardelli.lisa@gmail.com>
2020-12-07 16:38:57 +0000

    Return HTML

--- app.rb
+++ app.rb
@@ -8,3 +8,11 @@ get '/' do
 get '/secret' do
   "Secret page123"
 end
+
+get '/cat' do
+    "<div> 
+    <div style='border: 3px solid blue'>
+     
+    <img src= 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*'>
+     </div>"
+  end
